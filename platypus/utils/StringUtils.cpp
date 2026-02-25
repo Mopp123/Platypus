57926b5856968530f4ff18f3bf291a564565934a
mbpp333 <keahonen@gmail.com>
2026-02-25 17:49:05 +0200

    Added func to reverse utf8 strings

--- platypus/utils/StringUtils.cpp
+++ platypus/utils/StringUtils.cpp
@@ -60,6 +60,25 @@ namespace platypus { namespace util { namespace str {
         return (uint32_t)*it;
     }
 
+    std::string reverse(const std::string& str)
+    {
+        std::string result;
+        const size_t size = str.size();
+        char* pData = (char*)str.data();
+        utf8::iterator<char*> beginIt(pData, pData, pData + size);
+        utf8::iterator<char*> it(pData + size - 1, pData, pData + size);
+        while (true)
+        {
+            uint32_t codepoint = static_cast<uint32_t>(*it);
+            append_utf8(codepoint, result);
+            if (it == beginIt)
+                break;
+
+            --it;
+        }
+        return result;
+    }
+
     void iterate_utf8(
         const std::string& str,
         void* pUserData,
